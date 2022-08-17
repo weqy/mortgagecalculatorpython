90f5e1e28dce469c31dbe6313fc5704d77366425
weq <109391494+weqy@users.noreply.github.com>
2022-08-17 13:06:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,49 +2,32 @@ import tkinter as tk  # gui
 from tkinter import *
 
 ws = tk.Tk()  # canvas
-ws.title('fibonacci numbers python')  # canvas title
+ws.title('mortgage calculator weq')  # canvas title
 
 canvas1 = tk.Canvas(width=400, height=300)  # canvas height and width
 canvas1.pack()  # create canvas
 
-instructions = tk.Label(ws, text='Annual Interest Rate:', font=('helvetica', 10))  # instructions for user
-canvas1.create_window(120, 60, window=instructions)  # show instructions
-
-interest_val = IntVar()
-interest_rate = tk.Scale(ws, from_=0, to=7, orient=HORIZONTAL, variable=interest_val, showvalue=bool(0))
-canvas1.create_window(250, 65, window=interest_rate)  # shows entry in canvas
-interestLabel = tk.Label(ws, textvariable=interest_val)
-canvas1.create_window(310, 65, window= interestLabel)
-
-instructions2 = tk.Label(ws, text='Price of House:', font=('helvetica', 10))  # instructions for user
-canvas1.create_window(120, 100, window=instructions2)  # show instructions
-
-house_val = IntVar()
-house_price = tk.Scale(ws, from_=300000, to=600000, orient=HORIZONTAL, variable=house_val, showvalue=bool(0)) # create entry
-canvas1.create_window(250, 100, window=house_price)  # shows entry in canvas
-houseLabel = tk.Label(ws, textvariable=house_val)
-canvas1.create_window(325, 100, window= houseLabel)
-
-instructions3 = tk.Label(ws, text='Downpayment:', font=('helvetica', 10))  # instructions for user
-canvas1.create_window(120, 140, window=instructions3)  # show instructions
-
-payment_val = IntVar()
-downpayment = tk.Scale(ws, from_=100000, to=200000, orient=HORIZONTAL, variable=payment_val, showvalue=bool(0)) # create entry
-canvas1.create_window(250, 140, window=downpayment) # shows entry in canvas
-paymentLabel = tk.Label(ws, textvariable=payment_val)
-canvas1.create_window(325, 140, window= paymentLabel)
+money_range_x = [0, 300000, 100000, 2000]
+money_range_y = [7, 600000, 200000, 27000]
+user_instructions = ["Annual Interest Rate:", "Price of House:", "Downpayment:", "Yearly Property Tax:"]
+var_names = ["interest_val", "house_val", "payment_val", "tax_val"]
+widget_coordinates_x = [65, 100, 140, 180]
+label_names = ["interestLabel", "houseLabel", "paymentLabel", "taxLabel"]
+instruction_labels = ["instructions", "instructions2", "instructions3", "instructions4"]
+scale_names = ["interest_rate", "house_price", "downpayment", "yearly_tax"]
 
 
-instructions4 = tk.Label(ws, text='Yearly Property Tax:', font=('helvetica', 10)) # instructions for user
-canvas1.create_window(120, 180, window=instructions4) # show instructions
+x = 0
+for x in range(4):
+    instruction_labels[x] = tk.Label(ws, text=user_instructions[x], font=('helvetica', 10), width=16, anchor="e")
+    canvas1.create_window(120, widget_coordinates_x[x], window=instruction_labels[x])  # show instructions
+    var_names[x] = IntVar()
+    scale_names[x] = tk.Scale(ws, from_=money_range_x[x], to=money_range_y[x], orient=HORIZONTAL, variable=var_names[x], showvalue=bool(0))
+    canvas1.create_window(250, widget_coordinates_x[x], window=scale_names[x])  # shows entry in canvas
+    label_names[x] = tk.Label(ws, textvariable=var_names[x])
+    canvas1.create_window(330, widget_coordinates_x[x], window=label_names[x])
 
-tax_val = IntVar()
-yearly_tax = tk.Scale(ws, from_=2000, to=27000, orient=HORIZONTAL, variable=tax_val, showvalue=bool(0)) # create entry
-canvas1.create_window(250, 180, window=yearly_tax) # shows entry in canvas
-taxLabel = tk.Label(ws, textvariable=tax_val)
-canvas1.create_window(325, 180, window= taxLabel)
-
-
+    x += x+1
 
 ws.btn = tk.Button(ws, text = 'Enter', bd = '5', command= (lambda: create_loan())) # create button that runs welMsg
 canvas1.create_window(200, 260, window=ws.btn) # show button
@@ -55,10 +38,10 @@ def create_canvas(): # define function
     canvas1.delete("all")  # delete everything in canvas
 
 def create_loan():
-    P = float(house_price.get()) - float(downpayment.get())
+    P = float(scale_names[1].get()) - float(scale_names[2].get())
     months = 30 * 12
-    rate = float(interest_rate.get())/100
-    tax_yearly = float(yearly_tax.get())
+    rate = float(scale_names[0].get())/100
+    tax_yearly = float(scale_names[3].get())
 
 
     create_canvas()
